37e79e25f51d55e3d3b2ffafdb0df0d6e8010286
Jason Hwee <1216418+hweej@users.noreply.github.com>
2026-04-01 16:12:59 -0400

    feat: add auth stub module and openapi export script

---
+++ packages/api/src/cell_explorer_api/auth/__init__.py
@@ -0,0 +1,6 @@
+"""Authentication module (Keycloak OAuth2 + CloudFront signed cookies).
+
+Not yet implemented. This module will provide:
+- Keycloak OAuth2 login/logout flows
+- CloudFront signed cookie minting for protected Zarr access
+"""
